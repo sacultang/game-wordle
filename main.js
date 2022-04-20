2015b3299297e98ee261a3ff5876d6238e635677
sacultang <sacultang@gmail.com>
2022-04-21 00:18:10 +0900

    feat : add Button

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 let buttons= Array.from(document.querySelectorAll('.game-keyboard .key-btn'))
+// console.log(buttons)
+const delBtn = document.querySelector('.del')
+// console.log(delBtn)
+const enterBtn = document.querySelector('.enter')
 
 let word = 'APPLE' // 정답 단어
 // console.log(word.length)
+// word.toUpperCase
 let gameOver = false;
 
 let boardRow = 0; // 보드 줄 초기값
@@ -9,21 +14,65 @@ let tileNum = 0; // 보드 칸 초기값
 
 // // 현재 줄의 데이터 값
 let rowArr = document.querySelectorAll('.game-board')
-console.log(rowArr)
+// console.log(rowArr)
 // 현재 줄 안의 칸 데이터 값
 // let tileArr = Array.from(document.querySelectorAll('.board__text'))
 
 
+
+/* 버튼 이벤트 */
+buttons.map(e => {
+  e.addEventListener('click',(btn)=>{
+    // console.log(btn.target)
+    let btnTarget = btn.target
+    // console.log(btnTarget)
+    let btnText = btn.target.textContent
+    let upperCaseBtn = btnText.toUpperCase()
+    // console.log(upperCaseBtn)
+    if(btnTarget !== delBtn && btnTarget !== enterBtn){
+      if(tileNum < word.length){
+        let currTile = rowArr[boardRow].children[tileNum] 
+        if(currTile.textContent === ''){
+          currTile.textContent = upperCaseBtn
+          tileNum += 1;
+        }
+      }
+    }
+    else if (btnTarget === delBtn){
+      if(tileNum > 0 && tileNum <= word.length){
+        tileNum -=1;
+        console.log(tileNum)
+      }
+      let currTile = rowArr[boardRow].children[tileNum]
+      currTile.textContent = ''
+    } 
+    else if (btnTarget === enterBtn && tileNum === word.length){
+      gameCheck()
+      boardRow += 1;
+      tileNum = 0;
+      let currRow = rowArr[boardRow] 
+      
+    }
+  })
+})
+
+
+
+
+/* 키보드 이벤트 */
 document.addEventListener('keyup', e =>{
-  // console.log(e.code)
+  console.log(e)
+  console.log(e.code)
   // console.log(e.code[3])
   // console.log(e.key.toUpperCase())
   
-  if(gameOver) return;
-  if('KeyA' <= e.code && 'KeyZ' >= e.code){
+  if(gameOver) return; // gameover 시 리턴
+
+
+  if('KeyA' <= e.code && 'KeyZ' >= e.code){ // keycode 범위 지정해서 a-z까지만 누를수있게 지정한다
     // console.log(e.code)
     if(tileNum < word.length){ // 보드칸의 넘버가 단어 길이보다 작을 경우
-      let currTile = rowArr[boardRow].children[tileNum] // 현재 타일
+      let currTile = rowArr[boardRow].children[tileNum] // 현재 타일 
       if(currTile.innerText === ''){
         currTile.innerText = e.code[3]
         tileNum += 1;
@@ -39,7 +88,6 @@ document.addEventListener('keyup', e =>{
     }
     let currTile = rowArr[boardRow].children[tileNum]
     currTile.innerText = ''
-    
   }
   else if('Enter' === e.code && tileNum === word.length){
     gameCheck()
@@ -47,7 +95,6 @@ document.addEventListener('keyup', e =>{
     tileNum = 0;
     let currRow = rowArr[boardRow] 
     console.log(currRow)
-    
   }
 })
 
